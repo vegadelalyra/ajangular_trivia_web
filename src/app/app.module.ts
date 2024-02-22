0a1306ecd14d14bc751f75c71b028f92c01c7ea1
Vegadelalyra <lyradeperseo@gmail.com>
2024-02-21 22:10:36 -0500

    feat: trivia web pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio'
-
+import { MatButtonModule } from '@angular/material/button'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TriviaComponent } from './trivia/trivia.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 @NgModule({
   declarations: [
@@ -23,6 +25,13 @@ import { TriviaComponent } from './trivia/trivia.component';
     HttpClientModule,
     FormsModule,
     MatRadioModule,
+    MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/trivia/trivia.component.ts
+++ src/app/trivia/trivia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TriviaService } from '../trivia.service';
 import { Question, QuestionArray, Answer } from '../trivia';
+import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-trivia',
@@ -11,6 +12,10 @@ export class TriviaComponent implements OnInit {
   triviaData: QuestionArray = [];
   question: Question | null = null;
   answer: Answer | null = null;
+  disableRadioButtons: boolean = false;
+  disableNextButton: boolean = true;
+  questionNumber: number = 0;
+  correctAnswers: number = 0;
 
   constructor(private triviaService: TriviaService) { }
 
@@ -33,12 +38,22 @@ export class TriviaComponent implements OnInit {
   getNextQuestion() {
     if (this.triviaData.length) {
       const index = Math.floor(Math.random() * this.triviaData.length);
-
-      this.question = this.triviaData[index]
+      this.question = this.triviaData[index];
       this.triviaData.splice(index, 1);
     } else {
       this.question = null;
     }
+
+    if (this.answer) {
+      this.questionNumber++;
+      if (this.answer.is_correct) {
+        this.correctAnswers++;
+      }
+    }
+
+    this.answer = null;
+    this.disableRadioButtons = false;
+    this.disableNextButton = true;
   }
 
   getCorrectAnswer() {
@@ -46,5 +61,10 @@ export class TriviaComponent implements OnInit {
       return this.question.answers.filter(answer => answer.is_correct)[0].answer
     }
     return ''
+  }
+
+  answerSelected(event: Event) {
+    this.disableRadioButtons = true;
+    this.disableNextButton = false;
   }
 }
